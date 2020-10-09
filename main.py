82952174f8045f62e2bc83868b9b19b0b540a82e
Denwa799 <69731012+Denwa799@users.noreply.github.com>
2020-10-09 13:14:51 +0300

    release V1

---
+++ main.py
@@ -0,0 +1,65 @@
+def error():
+    input("Ошибка. Введите пожалуйста номер корректно. Для возврата в меню нажмите любую кнопку ")
+    menu()
+
+
+def task1():
+    nums = list(range(1, 1000))
+    length = len(nums)
+    i = 0
+    addition = 0
+    while i < length:
+        if nums[i] % 3 == 0 or nums[i] % 5 == 0:
+            addition += nums[i]
+        i += 1
+    print("Сумма чисел от 1 до 1000, кратных 3 или 5 =", addition)
+    input("Для возврата в меню нажмите любую кнопку ")
+    menu()
+
+
+def task2():
+    fib1 = 0
+    fib2 = 0
+    fib3 = 1
+    addition = 0
+    while fib3 < 3000000:
+        fib1 = fib2
+        fib2 = fib3
+        fib3 = fib1 + fib3
+        if fib3 % 2 == 0:
+            addition += fib3
+    print("Сумма четных чисел Фибоначчи ниже 4 миллионов =", addition)
+    input("Для возврата в меню нажмите любую кнопку ")
+    menu()
+
+
+def task3():
+    def simpleDividers(n):
+        answer = 0
+        d = 2
+        while d * d <= n:
+            if n % d == 0:
+                answer = d
+                n //= d
+            else:
+                d += 1
+        if n > 1:
+            answer = n
+        return answer
+
+    print("Cамый большой делитель числа 600851475143, являющийся простым числом=", simpleDividers(600851475143))
+    input("Для возврата в меню нажмите любую кнопку ")
+    menu()
+
+
+def menu():
+    print("Введите номер упражнения (от 1 до 3)")
+    task = input()
+    return {
+        "1": task1,
+        "2": task2,
+        "3": task3,
+    }.get(task, error)()
+
+
+menu()
